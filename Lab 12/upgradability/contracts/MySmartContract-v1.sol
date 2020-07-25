88d8196701b998e3bf4f910af8aee012b50407a7
mardel011 <59632640+mardel011@users.noreply.github.com>
2020-07-25 16:00:39 -0400

    Adding files

---
+++ Lab 12/upgradability/contracts/MySmartContract-v1.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+/**
+ * @dev Renamed version 1 to MySmartContractV1 to quickly workaround Truffle's singleton feature when deploying contracts.
+ * This allows us to have both contract versions deployed at the same time.
+ */
+contract MySmartContractV1 {
+    uint32 public counter;
+    bool private stopped = false;
+    address private owner;
+    /**
+  @dev Checks if the contract is not stopped; reverts if it is.
+  */
+    modifier isNotStopped {
+        require(!stopped, "Contract is stopped.");
+        _;
+    }
+    /**
+  @dev Enforces the caller to be the contract's owner.
+  */
+    modifier isOwner {
+        require(msg.sender == owner, "Sender is not owner.");
+        _;
+    }
+
+    constructor() public {
+        counter = 0;
+        // Sets the contract's owner as the address that deployed the contract.
+        owner = msg.sender;
+    }
+
+    /**
+  @notice Increments the contract's counter if contract is active.
+  @dev It will revert is the contract is stopped. See modifier "isNotStopped"
+   */
+    function incrementCounter() public isNotStopped {
+        counter += 2; // This is an intentional bug.
+    }
+
+    /**
+  @dev Stops / Unstops the contract.
+   */
+    function toggleContractStopped() public isOwner {
+        stopped = !stopped;
+    }
+}
---
+++ Lab 12/upgradability/contracts/MySmartContract-v2.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+contract MySmartContract {
+    uint32 public counter;
+    bool private stopped = false;
+    address private owner;
+    /**
+  @dev Checks if the contract is not stopped; reverts if it is.
+  */
+    modifier isNotStopped {
+        require(!stopped, "Contract is stopped.");
+        _;
+    }
+    /**
+  @dev Enforces the caller to be the contract's owner.
+  */
+    modifier isOwner {
+        require(msg.sender == owner, "Sender is not owner.");
+        _;
+    }
+
+    constructor(uint32 _counter) public {
+        counter = _counter; // Allows setting counter's initial value on deployment.
+        // Sets the contract's owner as the address that deployed the contract.
+        owner = msg.sender;
+    }
+
+    /**
+  @notice Increments the contract's counter if contract is active.
+  @dev It will revert is the contract is stopped. See modifier "isNotStopped"
+   */
+    function incrementCounter() public isNotStopped {
+        counter++; // Fixes bug introduced in version 1.
+    }
+
+    /**
+  @dev Stops / Unstops the contract.
+   */
+    function toggleContractStopped() public isOwner {
+        stopped = !stopped;
+    }
+}
